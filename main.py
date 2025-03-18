e13013943372a54f15b5e27a5a5abcc05017bd0d
Theo Bob Massard <tbobm@protonmail.com>
2025-03-18 13:13:00 +0100

    feat(main): add minimal working example
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-from typing import Union
+import os
+import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+import psycopg2
+from redis import StrictRedis
 
 app = FastAPI()
+redis = StrictRedis.from_url(os.environ["REDIS_URL"])
+logger = logging.getLogger('uvicorn.error')
+
+def get_db():
+    """Instantiate a connection to the Database."""
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
 
 @app.get("/health")
-def read_root():
+def simple_health_check():
     return {"message": "healthy"}
+
+
+@app.get("/health-full")
+def complete_health_check(response: Response):
+    try:
+        redis.ping()
+    except Exception as err:
+        logger.error('could not ping Redis %s', err)
+        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        return {
+            "message": "complete health check failed", 
+            "reason": "redis unreachable",
+        }
+
+    try:
+        with get_db() as conn:
+            with conn.cursor() as curs:
+                curs.execute("SELECT 1;")
+                logger.debug(curs.fetchone())
+    except Exception as err:
+        logger.error('could not access postgresql %s', err)
+        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        return {
+            "message": "complete health check failed", 
+            "reason": "postgres unreachable",
+        }
+    return {"message": "complete health check is healthy"}
 
 
 @app.get("/")
